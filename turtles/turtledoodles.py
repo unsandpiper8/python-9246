fda5941e418b09b975e031814c3654b1696e0782
unsandpiper8 <jcasselblad@gmail.com>
2026-04-03 15:24:44 +0200

    added turtledoodles

---
+++ turtles/turtledoodles.py
@@ -0,0 +1,51 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = []
+# ///
+
+# imports
+from random import randint
+from turtle import Turtle, Screen
+
+
+t1 = Turtle()
+# t2 = Turtle()
+screen = Screen()
+
+running = True
+
+
+def stop():
+    global running
+    running = False
+    print("quit")
+
+screen.listen()
+screen.onkey(stop, "q")
+
+
+t1.speed(0)
+
+while running:
+    t1.forward(randint(1, 20))
+    t1.right(randint(40, 90))
+    if t1.xcor() > 300:
+        t1.penup()   
+        t1.home()
+        t1.pendown()
+    if t1.xcor() < -300:
+        t1.penup()
+        t1.home()
+        t1.pendown()
+    if t1.ycor() > 300:
+        t1.penup()
+        t1.home()
+        t1.pendown()
+    if t1.ycor() < -300:
+        t1.penup()
+        t1.home()
+        t1.pendown()
+
+    screen.update()
+
+screen.mainloop()
