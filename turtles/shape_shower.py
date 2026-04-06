875d6162b8f6ef351f9b6a42e50cef78b15db439
unsandpiper8 <jcasselblad@gmail.com>
2026-04-06 17:13:10 +0200

    added new script: shape_shower.py

---
+++ turtles/shape_shower.py
@@ -0,0 +1,72 @@
+import turtle
+
+t = turtle
+
+def input_turtle_size():
+    global turtle_forward
+    turtle_forward = input("turtle size/forward: ")
+
+def input_turtle_shape():
+    global turtle_left
+    turtle_left = input("turtle shape/left: ")
+
+def turtle_size_checks():
+    try:
+        val1 = int(turtle_forward)
+    except ValueError:
+        print("err: not a valid value: has to be numeric")
+        input_turtle_size()
+    
+    if int(turtle_forward) > 400:
+        print("err: shape is to big for window")
+        input_turtle_size()
+
+def turtle_shape_checks():
+    try:
+        val2 = int(turtle_left)
+    except ValueError:
+        print("err: not a valid value: has to be numeric")
+        input_turtle_shape()
+
+def start_script():
+    t.resetscreen()
+    t.tracer(0)
+
+    t.teleport(-int_turtle_forward / 2, -int_turtle_left / 2)
+    t.begin_fill()
+    t.color("white")
+    t.fillcolor("black")
+
+    for _ in range(200):
+        t.fd(int_turtle_forward)
+        t.lt(int_turtle_left)
+        if abs(t.pos()) < 1:
+            break
+
+    t.end_fill()
+    t.update()
+
+def handle_quit_on_qpress():
+    screen.bye()
+
+
+print("enter shape attributes:")
+
+input_turtle_size()
+size_length = len(turtle_forward)
+turtle_size_checks()
+
+input_turtle_shape()
+shape_length = len(turtle_left)
+turtle_shape_checks()
+
+
+
+t.tracer(0)
+screen = turtle.Screen()
+int_turtle_forward = int(turtle_forward)
+int_turtle_left = int(turtle_left)
+start_script()
+screen.listen()
+screen.onkeypress(handle_quit_on_qpress, "q")
+t.done()
